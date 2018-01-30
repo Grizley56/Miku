5344828feedac74aad56fb77f01a6494be0c2e77
Ilya Meyta <Grizley.medved@gmail.com>
2018-01-30 23:11:20 +0300

    commented code removed

--- Miku.Framework/StringExtension.cs
+++ Miku.Framework/StringExtension.cs
@@ -13,30 +13,6 @@ namespace Miku.Framework
 	}
 	static class StringExtension
 	{
-
-		//internal static int IndexOf(this string source, string subString, int position = 0, Direction direction = Direction.LeftToRight)
-		//{
-		//	if (direction == Direction.LeftToRight)
-		//		return source.IndexOf(subString, position, StringComparison.Ordinal);
-
-		//	string reversed = source.Reverse();
-		//	int newPosition = source.Length - position;
-		//	int resultIndex = reversed.IndexOf(subString, newPosition, StringComparison.Ordinal);
-
-		//	return resultIndex == -1 ? -1 : source.Length - resultIndex;
-		//}
-
-		//internal static int IndexOfAny(this string source, char[] checkingChars, int position = 0, Direction direction = Direction.LeftToRight)
-		//{
-		//	if (direction == Direction.LeftToRight)
-		//		return source.IndexOfAny(checkingChars, position);
-
-		//	string reversed = source.Reverse();
-		//	int newPosition = source.Length - position;
-		//	int result = reversed.IndexOfAny(checkingChars, newPosition);
-		//	return result == -1 ? -1 : source.Length - result;
-		//}
-
 		internal static string Reverse(this string source)
 		{
 			return new string(source.ToCharArray().Reverse().ToArray());
